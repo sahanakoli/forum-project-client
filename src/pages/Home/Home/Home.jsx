d3dcf81b4c1d875e422e6e90fa0c4361ea3f609d
Sahana Koli <sahanasultanakoli@gmail.com>
2023-11-23 23:11:40 +0600

    navbar added

---
+++ src/pages/Home/Home/Home.jsx
@@ -0,0 +1,12 @@
+import Navbar from "../../../Shared/Navbar/Navbar";
+
+const Home = () => {
+    return (
+        <div>
+            <Navbar></Navbar>
+            <h2>Home Page</h2>
+        </div>
+    );
+};
+
+export default Home;
